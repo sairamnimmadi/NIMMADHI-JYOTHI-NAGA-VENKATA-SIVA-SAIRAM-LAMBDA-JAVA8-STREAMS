a1e816ddab263f3a0583c518fb57579cd2ec2144
sairam <sairamnimmadi@gmail.com>
2020-07-07 15:15:58 +0530

    Small changes

--- lambda_java8/src/main/java/com/maven/lambda_java8/App.java
+++ lambda_java8/src/main/java/com/maven/lambda_java8/App.java
@@ -14,34 +14,43 @@ public class App
     public static void main( String[] args )
     {
     	Scanner in = new Scanner(System.in);
-    	System.out.println("This Class has 3 Functinalities");
-    	System.out.println("1) For Average of a list of numbers : Press 1");
-    	System.out.println("2) For filter List of Strings Starts with 'a' and length of String equal to 3 : Press 2");
-    	System.out.println("3) For given List of Strings filter the Palindrome strings : press 3");
-    	int option = in.nextInt();
-    	System.out.println("Enter Size of the list");
-		int siz = in.nextInt();
-    	if(option == 1){
-    		List<Integer> list = new ArrayList<>();
-    		for(int i = 0;i<siz;i++) list.add(in.nextInt());
-    		Average avg = new Average();
-    		System.out.println("The average of the list you entered is "+ Double.toString(avg.avgList(list)));
+    	boolean flag = true;
+    	while(flag) {
+    		System.out.println("This Class has 3 Functinalities");
+        	System.out.println("1) For Average of a list of numbers : Press 1");
+        	System.out.println("2) For filter List of Strings Starts with 'a' and length of String equal to 3 : Press 2");
+        	System.out.println("3) For given List of Strings filter the Palindrome strings : press 3");
+        	System.out.println("4) For EXIT the program : press 4");
+        	int option = in.nextInt();
+        	if(option!=4) System.out.println("Enter Size of the list");
+        	if(option == 1){
+        		int siz = in.nextInt();
+        		List<Integer> list = new ArrayList<>();
+        		for(int i = 0;i<siz;i++) list.add(in.nextInt());
+        		Average avg = new Average();
+        		System.out.println("The average of the list you entered is "+ Double.toString(avg.avgList(list)));
+        	}
+        	else if(option == 2) {
+        		int siz = in.nextInt();
+        		List<String> list = new ArrayList<>();
+        		for(int i = 0;i<siz;i++) list.add(in.next());
+        		IdentifyString obj = new IdentifyString();
+        		System.out.print("The final list of filtered Strings is ");
+        		System.out.println(obj.filter(list));
+        		
+        	}
+        	else if(option == 3){
+        		int siz = in.nextInt();
+        		List<String> list = new ArrayList<>();
+        		for(int i = 0;i<siz;i++) list.add(in.next());
+        		List<String> result = filter(list, StringPalindrome::check_palindrome);
+        		System.out.print("The final list of palindrome Strings is ");
+        		System.out.println(result);
+        	}
+        	else flag = false;
+        	System.out.println();
     	}
-    	else if(option == 2) {
-    		List<String> list = new ArrayList<>();
-    		for(int i = 0;i<siz;i++) list.add(in.next());
-    		IdentifyString obj = new IdentifyString();
-    		System.out.print("The final list of filtered Strings is ");
-    		System.out.println(obj.filter(list));
-    		
-    	}
-    	else {
-    		List<String> list = new ArrayList<>();
-    		for(int i = 0;i<siz;i++) list.add(in.next());
-    		List<String> result = filter(list, StringPalindrome::check_palindrome);
-    		System.out.print("The final list of palindrome Strings is ");
-    		System.out.println(result);
-    	}
+    	
     	in.close(); 
     }
 }
